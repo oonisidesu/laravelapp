d18d4fb874ee98453ec72358dbfe7aea6f306fd4
otsubo <eeeelllleeee24@gmail.com>
2019-08-24 19:30:12 +0900

    otherアクションの追加

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}/{pass?}', 'HelloController@index') ;
+Route::get('hello', 'HelloController@index') ;
+Route::get('hello/other', 'HelloController@other') ;
+
