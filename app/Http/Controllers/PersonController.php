b82ae4c73fc40e17bc753e41655f3bde68311bfe
otsubo <eeeelllleeee24@gmail.com>
2019-09-04 22:19:30 +0900

    DBに追加機能をつけた

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -25,4 +25,21 @@ class PersonController extends Controller
         $param =['input' => $request->input, 'item' => $item];
         return view('person.find', $param);
     }
+
+    public function add(Request $request){
+        return view('person.add');
+    }
+
+    public function create(Request $request){
+        $this->validate($request, Person::$rules);
+        $person = new Person;
+        $form = $request->all();
+        unset($form['_token']);
+        //$person->fill($form)->save();
+        $person->name = $request->name;
+        $person->mail = $request->mail;
+        $person->age = $request->age;
+        $person->save();
+        return redirect('/person');
+    }
 }
