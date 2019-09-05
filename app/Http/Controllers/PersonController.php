e7f370fca2981b953ccfb8e8987a4210024936f6
otsubo <eeeelllleeee24@gmail.com>
2019-09-05 21:36:04 +0900

    編集機能を追加

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -42,4 +42,22 @@ class PersonController extends Controller
         $person->save();
         return redirect('/person');
     }
+
+    public function edit(Request $request){
+        $person = Person::find($request->id);
+        return view('person.edit',['form' => $person]);
+    }
+
+    public function update(Request $request){
+        $this->validate($request, Person::$rules);
+        $person = Person::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        //$person->fill($form)->save();
+        $person->name = $request->name;
+        $person->mail = $request->mail;
+        $person->age = $request->age;
+        $person->save();
+        return redirect('/person');
+    }
 }
